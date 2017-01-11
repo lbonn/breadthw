16f9535f2e747ce276e49c24517d5fad46069596
lbonn <bonnans.l@gmail.com>
2017-01-11 21:47:54 +0100

    Try converting to Protolude and Text
    Ugly as hell for now, conversions everywhere

--- src/Main.hs
+++ src/Main.hs
@@ -1,89 +1,101 @@
 module Main where
 
+import           Protolude hiding ((<>))
+
 import           Control.Exception
 import           Control.Monad
 import qualified Data.List           as L
-import           Data.Sequence       (Seq, (><))
+import           Data.Monoid
+import           Data.Sequence (Seq, (><))
 import qualified Data.Sequence       as Seq
-import           Options.Applicative
-import           Pipes
+import qualified Data.Text           as T
+import qualified Data.Text.IO
+import qualified Options.Applicative as OA
+import           Pipes               hiding (for)
+import qualified Pipes               as P
 import           System.Directory
 import           System.FilePath
-import           System.IO
+import           System.IO (hPutStrLn)
 
 import           GHC.Unicode
 
 
+type TextPath = Text
 data FileType = FTDir | FTFile | FTBoth deriving Show
 
 data Opts = Opts {
   fileTypes  :: FileType,
   skipHidden :: Bool,
-  startDir   :: FilePath
+  startDir   :: TextPath
 }
 
-parseFileType :: Monad m => String -> m FileType
+parseFileType ::  Monad m => [Char] -> m FileType
 parseFileType "d" = return FTDir
 parseFileType "f" = return FTFile
 parseFileType "a" = return FTBoth
 parseFileType _   = fail "invalid type"
 
-optsP :: Parser Opts
+optsP :: OA.Parser Opts
 optsP = Opts
-  <$> option (str >>= parseFileType)
-      ( long "file-type"
-     <> short 't'
-     <> help "File types to return (a|f|d)"
-     <> value FTBoth
-     <> metavar "FT" )
-  <*> switch
-      ( long "skip-hidden"
-     <> short 's'
-     <> help "Whether to walk into hidden dirs/files" )
-  <*> argument str
-      ( metavar "DIR"
-     <> help "Starting directory"
-     <> value "." )
+  <$> OA.option (OA.str >>= parseFileType)
+      ( OA.long "file-type"
+     <> OA.short 't'
+     <> OA.help "File types to return (a|f|d)"
+     <> OA.value FTBoth
+     <> OA.metavar "FT" )
+  <*> OA.switch
+      ( OA.long "skip-hidden"
+     <> OA.short 's'
+     <> OA.help "Whether to walk into hidden dirs/files" )
+  <*> OA.argument (map T.pack OA.str)
+      ( OA.metavar "DIR"
+     <> OA.help "Starting directory"
+     <> OA.value (T.pack ".") )
 
-matchFt :: FileType -> FilePath -> IO Bool
-matchFt FTDir  = doesDirectoryExist
-matchFt FTFile = doesFileExist
+matchFt :: FileType -> Text -> IO Bool
+matchFt FTDir  = doesDirectoryExist . T.unpack
+matchFt FTFile = doesFileExist . T.unpack
 matchFt FTBoth = \_ -> return True
 
-isHidden :: FilePath -> Bool
-isHidden p = case takeFileName p of
+-- TODO: reimplement
+isHidden :: Text -> Bool
+isHidden p = case takeFileName $ T.unpack p of
   '.' : _ : _ -> True
   _           -> False
 
-escapeWronglyEncoded :: String -> String
-escapeWronglyEncoded = map esc where
+escapeWronglyEncoded :: Text -> Text
+escapeWronglyEncoded = T.map esc where
   esc c = if isPrint c then c
     else '\65533'  -- Unicode REPLACEMENT CHARACTER
 
-matchOutputConds :: Opts -> FilePath -> IO Bool
+matchOutputConds :: Opts -> Text -> IO Bool
 matchOutputConds opts p =
-  if not $ all isPrint p then do
-    hPutStrLn stderr $ "bad encoding: " ++ escapeWronglyEncoded p
+  if not $ T.all isPrint p then do
+    hPutStrLn stderr . T.unpack $ "bad encoding: " `mappend` escapeWronglyEncoded p
     return False
   else do
     let fnMatch = not (skipHidden opts) || not (isHidden p)
     if fnMatch then matchFt (fileTypes opts) p else return False
 
 
-walkOnce :: FilePath -> IO [FilePath]
+walkOnce :: Text -> IO [Text]
 walkOnce p = do
-  isDir <- doesDirectoryExist p
+  isDir <- doesDirectoryExist up
   if not isDir then return [] else do
-    l <- try $ listDirectory p :: IO (Either IOException [FilePath])
+    l <- try $ listDirectory up :: IO (Either IOException [FilePath])
 
     case l of
       Left e -> do
-        hPrint stderr e
+        hPutStrLn stderr (T.unpack . show $ e)
         return []
-      Right rl -> return $ map (combine p) (L.sort rl)
+      Right rl -> return $ map ((T.pack . combine up) . T.unpack)  (L.sort trl)
+        where
+          trl = map T.pack rl
+  where
+    up = T.unpack p
 
 
-walkDir :: Opts -> Seq FilePath -> Producer FilePath IO ()
+walkDir :: Opts -> Seq Text -> Producer Text IO ()
 walkDir opts = walkd where
   walkd q = case Seq.viewl q of
     Seq.EmptyL -> return ()
@@ -96,17 +108,17 @@ walkDir opts = walkd where
       walkd $ xs >< Seq.fromList children
 
 
-formatPath :: FilePath -> String
-formatPath = normalise
+formatPath :: Text -> Text
+formatPath = T.pack . normalise . T.unpack
 
 mainWalk :: Opts -> IO ()
-mainWalk opts = runEffect $ for walk disp
+mainWalk opts = runEffect $ P.for walk disp
   where
     walk = walkDir opts (Seq.fromList [startDir opts])
     disp = lift . putStrLn . formatPath
 
 
 main :: IO ()
-main = execParser opts >>= mainWalk
+main = OA.execParser opts >>= mainWalk
   where
-    opts = info (helper <*> optsP) fullDesc
+    opts = OA.info (OA.helper <*> optsP) OA.fullDesc
