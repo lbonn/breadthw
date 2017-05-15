26464aa3e4e7ff0411bd65982abcbccecc35dc4b
lbonn <bonnans.l@gmail.com>
2017-05-15 23:34:15 +0200

    test: convert to tasty

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,14 +1,18 @@
 module Main where
 
 import           Protolude
-import           Test.HUnit
+-- import           Test.HUnit
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.QuickCheck
 
-import           Data.Maybe
+import           Data.Maybe       (fromJust)
 
 import           Data.Sequence    (fromList)
 
 import           Breadthw.ZipTree
 
+-- regular unit tests
 leaf :: a -> Tree a
 leaf e = Node e empty
 
@@ -16,27 +20,41 @@ bigTree :: Tree Int
 bigTree = Node 1 $ fromList [Node 2 (fromList [leaf 4, leaf 5, Node 6 $ fromList [leaf 9, leaf 10]])
                            , Node 3 (fromList [leaf 7, Node 8 $ fromList [leaf 11]])
                             ]
-
 bigZt :: ZipTree Int
 bigZt = fromTree bigTree
 
 viewRoot :: ZipTree a -> a
 viewRoot = root . view
 
-tests :: Test
-tests = TestList
-  [
-    "go far left" ~:
-      let z = fromJust $ goPath [1, 1] bigZt in
-          assertEqual "far left" 4 $ viewRoot . fromJust $ goDepthFarLeft z 2
+hTests :: TestTree
+hTests = testGroup "Unit tests"
+  [ testCase "go far left" (
+    let z = fromJust $ goPath [1, 1] bigZt in
+    assertEqual "far left" 4 $ viewRoot . fromJust $ goDepthFarLeft z 2
+  )
 
-  , "breadth-next root" ~:
-      assertEqual "breadth next" 2 (viewRoot . fromJust $ breadthNext bigZt)
+  , testCase "breadth-next root" (
+    assertEqual "breadth next" 2 (viewRoot . fromJust $ breadthNext bigZt)
+  )
 
-  , "breadth-first traversal" ~:
-      let traversal = foldr (\e l -> (root . view) e : l) [] (accum breadthNext bigZt) in
-          assertEqual "breadth traversal" [1,2,3,4,5,6,7,8,9,10,11] traversal
+  , testCase "breadth-first traversal" (
+    let traversal = foldr (\e l -> (root . view) e : l) [] (accum breadthNext bigZt) in
+    assertEqual "breadth traversal" [1,2,3,4,5,6,7,8,9,10,11] traversal
+  )
   ]
 
+-- quickcheck
+
+qTests :: TestTree
+qTests = testGroup "Quick checks"
+  [
+  ]
+
+
+tests :: TestTree
+tests =  testGroup "Tests" [ hTests
+                           , qTests
+                           ]
+
 main :: IO ()
-main = void $ runTestTT tests
+main = defaultMain tests
